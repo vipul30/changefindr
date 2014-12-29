70cc48e60862d28c36417cfa5fc9503d57753a54
vipul30 <vipul30@gmail.com>
2014-12-29 15:15:31 -0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root :to => "home#index"
+  match ':controller(/:action(/:id))', :via => :get
+
+
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
