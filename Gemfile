bb841b6a4cd28c4080f0efa07520b30f698ec287
vipul30 <vipul30@gmail.com>
2015-03-20 17:31:29 -0700

    integrating bhn

--- Gemfile
+++ Gemfile
@@ -85,6 +85,7 @@ gem 'rake-compiler', '~> 0.9.5'
 # this was needed for BHN certification
 gem 'httpi-ntlm', '~> 0.9.6'
 gem 'inifile', '~> 3.0.0'
+gem 'rubyntlm', '~> 0.5.0'
 
 gem 'rest-client', '~> 1.7.3'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,8 +26,40 @@ class HomeController < ApplicationController
   	   @giftcardstats = Giftcardstat.limit(3).order("RANDOM()")
     end
 
+      url = 'https://sandbox.blackhawknetwork.com/productManagement/v1/productLines?first=0&maximum=10&ascending=true&exactMatch=false&caseSensitive=false'
 
-      
+      request = HTTPI::Request.new('https://sandbox.blackhawknetwork.com/productManagement/v1/productLines?first=0&maximum=500&ascending=true&exactMatch=false&caseSensitive=false')
+
+      request.auth.ssl.cert_key_file = "key.pem"
+      request.auth.ssl.cert_file = "key.pem"
+      request.headers["requestorId"] = "YGSZRJHZC9KA1VNV62C9ARGSF4"
+      request.headers["Content-Type"] =  "application/json; charset=UTF-8"
+      request.headers["Accept"] = "application/json; charset=UTF-8"
+      request.open_timeout = 29
+      request.read_timeout = 29
+
+      response = HTTPI.get request
+    
+      resultcode = response.code
+
+      results = JSON.parse response.raw_data
+      giftcards = results['results']  # this is the json result to get the gift cards
+
+=begin
+  (byebug) results['results'][0]
+{"entityId"=>"https://sandbox.blackhawknetwork.com/productManagement/v1/productLine/1N8ZBLNAW6SJPNBBL2SMJSC4GH", "productLineName"=>"Sephora", "brandId"=>"KNVD2RCXPDW8M2JAYSRHR2A9R4", "brandName"=>"Sephora", "brandCode"=>"LVMH", "brandLogoImage"=>"https://content.giftcardmall.com/gcmimages/manufacturer/icon/GOWALLET_METADATA/186V2565.0.PNG", "productLineStatus"=>"ACTIVATED", "accountType"=>"GIFT_CARD", "startDate"=>"2013-06-21T22:53:14.000+0000", "endDate"=>"2020-02-01T13:22:54.000+0000", "locale"=>"en_US"}
+(byebug) results['results'][0]['entityId']
+"https://sandbox.blackhawknetwork.com/productManagement/v1/productLine/1N8ZBLNAW6SJPNBBL2SMJSC4GH"
+(byebug) 
+
+=end
+  
+end
+
+
+byebug
+
+
 
   end
 
