96520f6a4c6a6092bf8ff8187812bfd6659af41b
vipul30 <vipul30@gmail.com>
2015-04-07 16:34:12 -0400

    updated for merchant logo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,9 +115,8 @@ config.action_mailer.raise_delivery_errors = true
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  },
-  :url =>ENV['AWS_URL'],
-  :path => ENV['AWS_PATH']
+  }
+  
 }
 
 
