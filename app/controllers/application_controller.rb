f5a5e2542479598680fbf6ab6275f5ed259929ca
vipul30 <vipul30@gmail.com>
2015-04-14 09:14:05 -0700

    testing bhn cert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -90,13 +90,13 @@ require 'aws-sdk'
         curl.headers['contractId'] = ENV['bhn_contractId_preprod']
 
         #s3 = Aws::S3::Client.new
-        curl.cert = ENV['bhn_cert_preprod']
+        curl.cert = Rails.root.join('certs/cert_preprod.p12').to_s #ENV['bhn_cert_preprod']
         #s3.get_object({ bucket:ENV['S3_BUCKET_NAME'], key:ENV['AWS_ACCESS_KEY_ID'] }, target: '/certs/cert_preprod.p12') #'certs/cert_preprod.p12' #ENV['bhn_cert_preprod']
         
         
 
 
-        curl.cert_key = ENV['bhn_cert_pass_file_preprod']
+        curl.cert_key = Rails.root.join('certs/cert_preprod.pw').to_s #ENV['bhn_cert_pass_file_preprod']
         curl.certpassword = ENV['bhn_cert_password_preprod']
         
         curl.follow_location = true
