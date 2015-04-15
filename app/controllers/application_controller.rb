85eb60e0a2ba7d100487235e672529ddbbfe702b
vipul30 <vipul30@gmail.com>
2015-04-15 09:29:19 -0700

    testing preprod cert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -90,16 +90,21 @@ require 'aws-sdk'
         curl.headers['contractId'] = ENV['bhn_contractId_preprod']
 
         #s3 = Aws::S3::Client.new
-        curl.cert = 'testing' #'http://dpaqzkfinvd5q.cloudfront.net/certs/cert_preprod.p12' #Rails.root.join('cert/cert_sandbox.p12').to_s #ENV['bhn_cert_preprod']
+        #curl.cert = 'testing' #'http://dpaqzkfinvd5q.cloudfront.net/certs/cert_preprod.p12' #Rails.root.join('cert/cert_sandbox.p12').to_s #ENV['bhn_cert_preprod']
         #s3.get_object({ bucket:ENV['S3_BUCKET_NAME'], key:ENV['AWS_ACCESS_KEY_ID'] }, target: '/certs/cert_preprod.p12') #'certs/cert_preprod.p12' #ENV['bhn_cert_preprod']
+        curl.certtype = "PEM"
+        curl.cert = Rails.root.join('certs/key_preprod.pem').to_s #Rails.root.join(ENV['bhn_cert_preprod']).to_s
         
+
         errmessage = Bhnquote.new
         errmessage.created = Time.now
         errmessage.errorMessage = curl.cert
         errmessage.save
         
 
-        curl.cert_key = Rails.root.join(ENV['bhn_cert_pass_file_preprod']).to_s #ENV['bhn_cert_pass_file_preprod']
+        #curl.cert_key = Rails.root.join(ENV['bhn_cert_pass_file_preprod']).to_s #ENV['bhn_cert_pass_file_preprod']
+
+        curl.cert_key = Rails.root.join('certs/key_preprod.pem').to_s
 
         errmessage = Bhnquote.new
         errmessage.created = Time.now
