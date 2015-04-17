d7443d15ef1d1d5f97cab341f282001004ab2c53
vipul30 <vipul30@gmail.com>
2015-04-17 10:39:12 -0700

    modified code for bhn errors

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -66,15 +66,22 @@ class DonateController < ApplicationController
     @donation.created = Time.now
     @donation.modified = Time.now
 
-
-    if params[:donation][:giftcard_attributes][:giftcardid]
+    
+    
+    if !params[:donation][:giftcard_attributes][:giftcardid].empty?
       @donation.giftcard.giftcardid = params[:donation][:giftcard_attributes][:giftcardid]
       @donation.giftcardid = params[:donation][:giftcard_attributes][:giftcardid]
       
     else
+      if @donation.giftcard == nil
+        @donation.giftcard = Giftcard.new
+      end
+
       @donation.giftcard.created = Time.now
       @donation.giftcard.modified = Time.now
       @donation.giftcard.balancecheckdate = Time.now
+      
+      @donation.giftcard.merchantid = 85 # gift card not found
     end
     
     if @donation.giftcard.created == nil
@@ -84,6 +91,7 @@ class DonateController < ApplicationController
     end
    
     @giftcard = @donation.giftcard
+
 
     if session[:userid]
       @donation.userid = session[:userid]
@@ -97,43 +105,52 @@ class DonateController < ApplicationController
     # returns a Bhnquote model object
     merchant = Merchant.where(:merchantid => @donation.giftcard.merchantid).first
 
-    bhnquote = Bhnquote.new
-    bhnquote = getcardbalance(@donation.giftcard.cardnumber, @donation.giftcard.pin, merchant.productLineId)
+    if merchant.merchantid != 85
+      bhnquote = Bhnquote.new
+      bhnquote = getcardbalance(@donation.giftcard.cardnumber, @donation.giftcard.pin, merchant.productLineId)
 
 
+      if bhnquote.responsecode == '200' || bhnquote.responsecode == '201'
 
-    if bhnquote.responsecode == '200' || bhnquote.responsecode == '201'
+       @donation.giftcard.balance = bhnquote.actualCardValue
 
-     @donation.giftcard.balance = bhnquote.actualCardValue
-
-    else
-      # error
-      flash[:notice] = bhnquote.errorMessage
-      render('new') 
-      return
+      else
+        # error
+        # only display error message if invalid number of balance is 0, otherwise we will process on the backend
+        if bhnquote.errorMessage == 'The card number is invalid or there is no value on the card.'
+          flash[:notice] = bhnquote.errorMessage
+          render('new') 
+          return
+        end
+      end
     end
 
 
     if @donation.save
 
+      if merchant.merchantid != 85
+          # check if the product line is accepted by bhn 
+          # save the bhn quote
+          bhnquote.giftcardid = @donation.giftcard.giftcardid
+          bhnquote.save
 
-      # check if the product line is accepted by bhn 
+          # call the method to have bhn acquire the donation
+          bhnacquire = Bhnacquire.new
+          bhnacquire = bhnacquirecard(@donation)
+          
+          bhnacquire.save
 
-      # save the bhn quote
-      bhnquote.giftcardid = @donation.giftcard.giftcardid
-      bhnquote.save
-
-      # call the method to have bhn acquire the donation
-      bhnacquire = Bhnacquire.new
-      bhnacquire = bhnacquirecard(@donation)
+        flash[:notice] = 'Thank you for your donation in the amount of ' + number_to_currency(@donation.giftcard.balance).to_s + ' to ' + @donation.charity.charityname + '.  Please hold onto your gift card until you get an email from us informing you we have processed it.'
       
-      bhnacquire.save
+      else
+          flash[:notice] = 'Thank you for your donation to ' + @donation.charity.charityname + '.  Please hold onto your gift card until you get an email from us informing you we have processed it.'
+      
+      end
 
-
-      flash[:notice] = 'Thank you for your donation in the amount of ' + number_to_currency(@donation.giftcard.balance).to_s + ' to ' + @donation.charity.charityname + '.  Please hold onto your gift card until you get an email from us informing you we have processed it.'
       DonateMailer.donate_email(@donation, request.host_with_port).deliver
       redirect_to(:controller => "home", :action => "index")
       return
+
     else
 
       render('new')
