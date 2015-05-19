9f0ca6dcee61116d433d23ffccf3655fb3d15e3c
vipul30 <vipul30@gmail.com>
2015-05-19 10:45:59 -0700

    charity image upload fix

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -17,25 +17,55 @@ class Charity < ActiveRecord::Base
             					:bucket => 'changefindr.com',
 								:size => { :in => 0..500.kilobytes } }
 
-	has_attached_file :image1 , {	styles: {thumb: "60x60#"},
-									:path => "Users/vipul/Sites/changefindr/public/images/photo_attachment/:id/:style/:basename.:extension",
-									:url => "/images/photo_attachment/:id/:style/:basename.:extension",
-									:default_url => "/images/photo_attachment/missing_thumb.png",
-									:size => { :in => 0..500.kilobytes }
-								}
-
-	has_attached_file :image2, {	styles: {thumb: "60x60#"},
+	has_attached_file :image1 , {	styles: {thumb: "75x75#"},
 								:path => "Users/vipul/Sites/changefindr/public/images/photo_attachment/:id/:style/:basename.:extension",
 								:url => "/images/photo_attachment/:id/:style/:basename.:extension",
 								:default_url => "/images/photo_attachment/missing_thumb.png",
+								:storage => :s3,
+  								:s3_credentials => {
+								    :bucket => 'changefindr.com',
+								    :access_key_id => ACCESS_KEY_ID,
+								    :secret_access_key => SECRET_ACCESS_KEY
+								  },
+								:access_key_id => ACCESS_KEY_ID,
+								:secret_access_key => SECRET_ACCESS_KEY,
+            					:s3_host_name => 's3-us-west-1.amazonaws.com',
+            					:bucket => 'changefindr.com',
+								:size => { :in => 0..500.kilobytes }
+								}
+
+	has_attached_file :image2, {	styles: {thumb: "75x75#"},
+								:path => "Users/vipul/Sites/changefindr/public/images/photo_attachment/:id/:style/:basename.:extension",
+								:url => "/images/photo_attachment/:id/:style/:basename.:extension",
+								:default_url => "/images/photo_attachment/missing_thumb.png",
+								:storage => :s3,
+  								:s3_credentials => {
+								    :bucket => 'changefindr.com',
+								    :access_key_id => ACCESS_KEY_ID,
+								    :secret_access_key => SECRET_ACCESS_KEY
+								  },
+								:access_key_id => ACCESS_KEY_ID,
+								:secret_access_key => SECRET_ACCESS_KEY,
+            					:s3_host_name => 's3-us-west-1.amazonaws.com',
+            					:bucket => 'changefindr.com',
 								:size => { :in => 0..500.kilobytes }
 							    }
 
-	has_attached_file :image3, {styles: {thumb: "60x60#"},
+	has_attached_file :image3, {styles: {thumb: "75x75#"},
 								:path => "Users/vipul/Sites/changefindr/public/images/photo_attachment/:id/:style/:basename.:extension",
 								:url => "/images/photo_attachment/:id/:style/:basename.:extension",
 								:default_url => "/images/photo_attachment/missing_thumb.png",
-								:size => { :in => 0..600.kilobytes }
+								:storage => :s3,
+  								:s3_credentials => {
+								    :bucket => 'changefindr.com',
+								    :access_key_id => ACCESS_KEY_ID,
+								    :secret_access_key => SECRET_ACCESS_KEY
+								  },
+								:access_key_id => ACCESS_KEY_ID,
+								:secret_access_key => SECRET_ACCESS_KEY,
+            					:s3_host_name => 's3-us-west-1.amazonaws.com',
+            					:bucket => 'changefindr.com',
+								:size => { :in => 0..500.kilobytes }
 								 }
 
 	validates :charityname, :presence => { :message => "Please enter the name of your cause." },
